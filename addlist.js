9cc70c48d973302f3df74ecfb921755b108f6d0c
benkhazaal <thajukez101@gmail.com>
2024-02-23 16:32:01 -0700

    things are looking up baby

--- addlist.js
+++ addlist.js
@@ -1,5 +1,21 @@
 const BUTTON = document.getElementById('addlist');
+
 BUTTON.addEventListener('click', () => {
-    console.log('yes')
+
+    const P1 = document.createElement("p")
+    P1.textContent = "List Name:";
+    const INPUT1 = document.createElement("input");
+    const newlistDiv = document.getElementById('newlist');
+    newlistDiv.appendChild(P1);
+    newlistDiv.appendChild(INPUT1);
     
-});
+    const P2 = document.createElement("p")
+    P2.textContent = "Description:";
+    const INPUT2 = document.createElement("input");
+    newlistDiv.appendChild(P2);
+    newlistDiv.appendChild(INPUT2);
+
+
+    console.log('yay!');
+    
+},{once : true});
